ac584fcddb31edc5bb4614a6691ce2f71e448d50
GKPedrsen <GKPedrsen@gmail.com>
2024-11-19 14:47:12 +0100

    2015/3 initial solve for part 2

--- 2015/Day3/Day3.cs
+++ 2015/Day3/Day3.cs
@@ -6,7 +6,7 @@ namespace AOC2015
   {
     public string SolveFirst(string input)
     {
-      HashSet<(int, int)> visitedTiles =  new HashSet<(int, int)>();
+      HashSet<(int, int)> visitedTiles = new HashSet<(int, int)>();
       visitedTiles.Add( (0,0) );
 
       input.Aggregate((x: 0, y: 0), (pos, c) => 
@@ -23,7 +23,29 @@ namespace AOC2015
 
     public string SolveSecond(string input)
     {
-      throw new NotImplementedException();
+      HashSet<(int, int)> visitedTiles =  new HashSet<(int, int)>();
+      visitedTiles.Add( (0,0) );
+
+      input.Aggregate((sx: 0, sy: 0, rx: 0, ry: 0, counter:0), (acc, c) => 
+      {
+        if (acc.counter % 2 == 0)
+        {
+          acc.sx += c == '<' ? -1 : c == '>' ? +1 : 0;
+          acc.sy += c == '^' ? +1 : c == 'v' ? -1 : 0;
+          visitedTiles.Add((acc.sx, acc.sy));
+        }
+        else
+        {
+          acc.rx += c == '<' ? -1 : c == '>' ? +1 : 0;
+          acc.ry += c == '^' ? +1 : c == 'v' ? -1 : 0;
+          visitedTiles.Add((acc.rx, acc.ry));
+        }
+
+        ++acc.counter;
+        return acc; 
+      });
+
+      return visitedTiles.Count.ToString();
     }
   }
 }
