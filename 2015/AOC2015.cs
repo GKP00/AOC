592043125914153d15d3e8f0205a48d6195413a6
GKPedrsen <GKPedrsen@gmail.com>
2024-11-19 14:28:26 +0100

    AOC2015 handle NotImplementedException for solutions

--- 2015/AOC2015.cs
+++ 2015/AOC2015.cs
@@ -10,8 +10,14 @@ void ExecDay(int n)
 
   string input = File.ReadAllText($"{dayN}/input");
 
-  string s1; try { s1 = dayO!.SolveFirst(input);  } catch (InvalidInputException e) { s1 = $"\x1b[31m\"{e.Message}\"\x1b[0m"; };
-  string s2; try { s2 = dayO!.SolveSecond(input); } catch (InvalidInputException e) { s2 = $"\x1b[31m\"{e.Message}\"\x1b[0m"; };
+  string s1; 
+  string s2; 
+
+  try { s1 = dayO!.SolveFirst(input);  } 
+  catch (Exception e) when (e is InvalidInputException || e is NotImplementedException) { s1 = $"\x1b[31m\"{e.Message}\"\x1b[0m"; };
+
+  try { s2 = dayO!.SolveSecond(input); } 
+  catch (Exception e) when (e is InvalidInputException || e is NotImplementedException) { s2 = $"\x1b[31m\"{e.Message}\"\x1b[0m"; };
 
   Console.WriteLine($"{dayN,2}: ({s1}, {s2})");
 }
