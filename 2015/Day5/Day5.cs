ee6927207a01a7ee9a06060037d99e11a0d31670
GKPedrsen <GKPedrsen@gmail.com>
2024-11-27 23:50:10 +0100

    2015/5 part 2 initial solution

--- 2015/Day5/Day5.cs
+++ 2015/Day5/Day5.cs
@@ -8,17 +8,68 @@ namespace AOC2015
       int nNiceStrings = 0;
 
       foreach(string line in lines)
-        nNiceStrings += isNiceString(line) ? 1 : 0;
+        nNiceStrings += isNiceString1(line) ? 1 : 0;
 
       return nNiceStrings.ToString();
     }
 
     public string SolveSecond(string input)
     {
-      throw new NotImplementedException();
+      var lines = input.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+      int nNiceStrings = 0;
+
+      foreach (string line in lines)
+        nNiceStrings += isNiceString2(line) ? 1 : 0;
+
+      return nNiceStrings.ToString();
     }
 
-    private bool isNiceString(string str)
+    private bool isNiceString2(string str)
+    {
+      //could be 26 x 3 bytes if using bitfields but meh 
+      //gonna use a whole byte to store true / false
+      char[,] followedByMap = new char[26, 26];
+
+      bool hasSandwich  = false;
+      bool hasPairOfTwo = false;
+
+      //if on each iteration i updated the followedByMap immediately then the condition for
+      //hasPairOfTwo would be true in cases of overlapping pairs like 'aaa'
+      //to solve that i first store to a temp and then in the next iteration i store whatever is in temp to the map
+      //effectively skipping checks for the very last char iterated over
+      int tmpX = 0, tmpY = 0;
+
+      for(int i = 0; i < str.Length-1; ++i)
+      {
+        if (!hasSandwich && (i < str.Length - 2))
+        {
+          if (str[i] == str[i + 2])
+            hasSandwich = true;
+        }
+
+        if (str[i] < 'a' || str[i] > 'z')
+          throw new InvalidInputException("encountered non-lowercase or non-alpha input character, which this solution asserts.");
+
+        int thisChOffset = str[i]   - 'a';
+        int nextChOffset = str[i+1] - 'a';
+
+        if (followedByMap[thisChOffset, nextChOffset] == (char)1)
+          hasPairOfTwo = true;
+
+        if (hasSandwich && hasPairOfTwo)
+          return true;
+
+        if(i != 0)
+          followedByMap[tmpX, tmpY] = (char)1;
+
+        tmpX = thisChOffset;
+        tmpY = nextChOffset;
+      }
+
+      return false;
+    }
+
+    private bool isNiceString1(string str)
     {
       int vowels = 0;
       bool doubleLetter = false;
