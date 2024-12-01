8aee6d7488ceb1d5a42eefc61928dc2ba15d97a2
GKPedrsen <GKPedrsen@gmail.com>
2024-12-01 08:13:25 +0100

    2024/1 use unsigned & map (seeking definitely wasn't faster)

--- 2024/Day1/Day1.cpp
+++ 2024/Day1/Day1.cpp
@@ -2,22 +2,23 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 int main()
 {
   std::ifstream input{"./input.txt"};
 
-  std::vector<int> list1, list2;
+  std::vector<unsigned> list1, list2;
+  std::unordered_map<unsigned , unsigned> freqMap;
   while( input.peek() != EOF )
   {
-    int n1, n2;
+    unsigned n1, n2;
     input >> n1;
-    input.get(); //tab seperator
     input >> n2;
-    input.get(); //newline
 
     list1.emplace_back(n1);
     list2.emplace_back(n2);
+    ++freqMap[n2];
   }
 
   std::sort(list1.begin(), list1.end());
@@ -27,18 +28,11 @@ int main()
   unsigned simuScore = 0;
   for(size_t i = 0; i < list1.size(); ++i)
   {
-    totalDiff += std::abs(list1[i] - list2[i]);
+    totalDiff += list1[i] >= list2[i] ?
+                   list1[i] - list2[i]:
+                   list2[i] - list1[i];
 
-    //i hope seeking like this is actually faster than a map lookup...
-    size_t j = i;
-    //seek back to before first instance of value list1[i] (or lower) in list2
-    while(j > 0 && list2[j] >= list1[i]) --j;
-
-    //seek ahead to first instance of value list1[i] (or higher) in list2
-    while(j < list2.size() && list2[j] < list1[i]) ++j;
-
-    //add list1[i] as many times as it occurs in list2
-    while(list2[j] == list1[i] && j++ < list2.size()) simuScore += list1[i];
+    simuScore += list1[i] * freqMap[list1[i]];
   }
 
   std::cout << "p1: " << totalDiff << '\n';
